26f43b179013543fbfeb31a2e868205aa6845ac6
kakehi.keigo.3134@members.co.jp <kakehi.keigo.3134@members.co.jp>
2021-07-04 16:57:54 +0900

    LoginStaffPage作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { NewGuestPage } from './components/pages/NewGuestPage';
 import { LoginGuestPage } from './components/pages/LoginGuestPage';
+import { LoginStaffPage } from './components/pages/LoginStaffPage'
 
 function App() {
   return (
     <>
       <NewGuestPage />
       <LoginGuestPage />
+      <LoginStaffPage />
     </>
   );
 }
