87ca5a6f284a9cbafb01711d7cf617753bc81f6c
kakehi.keigo.3134@members.co.jp <kakehi.keigo.3134@members.co.jp>
2021-07-13 11:12:25 +0900

    grid適用

--- src/components/pages/ChatPage.tsx
+++ src/components/pages/ChatPage.tsx
@@ -1,20 +1,39 @@
 import React, { useEffect } from 'react'
 import { auth } from "../../firebase";
 import { useHistory } from 'react-router';
+import { ContactItems } from '../organisms/ContactItems';
+import { ContactInputField } from '../organisms/ContactInputField';
+import { makeStyles } from '@material-ui/core'
+import { useSelector } from "react-redux";
+import { selectUser } from "./../../features/userSlice";
+
+const useStyles = makeStyles({
+    root: {
+        display: 'grid',
+        height: '100vh',
+        gridTemplateRows: '1fr auto',
+    },
+});
 
 export const ChatPage:React.VFC = () => {
     const history = useHistory();
+    const classes = useStyles();
+    const user = useSelector(selectUser);
 
     useEffect(() => {
+        if(user.staff !== false)
         history.push('/')
     },[])
     return (
-        <div>
-            <p>チャットページです</p>
-            <button onClick={async () => {
-              await auth.signOut();
-              history.push('/');
-            }}>サインアウト</button>
+        <>
+        <div className={classes.root}>
+            <ContactItems />
+            <ContactInputField />
         </div>
+        <button onClick={async () => {
+            await auth.signOut();
+            history.push('/');
+          }}>サインアウト</button>
+        </>
     )
 }
