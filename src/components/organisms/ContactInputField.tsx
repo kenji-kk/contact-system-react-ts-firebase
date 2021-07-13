eafdd17cf7f5b089456b54695fa8aad7efeb15de
kakehi.keigo.3134@members.co.jp <kakehi.keigo.3134@members.co.jp>
2021-07-13 12:19:25 +0900

    Grid-material-ui作成

--- src/components/organisms/ContactInputField.tsx
+++ src/components/organisms/ContactInputField.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Grid } from '@material-ui/core'
+import { useSelector } from "react-redux";
+import { selectUser } from "./../../features/userSlice";
 
 const useStyles = makeStyles({
     root: {
         gridRow:2,
+        margin:'26px',
     },
 });
 
 export const ContactInputField = () => {
     const classes = useStyles();
+    const user = useSelector(selectUser);
 
     return (
         <div className={classes.root}>
-            
+            <Grid container>
+                <Grid xs={4}>{user.staff ? "スタッフ入力フォーム" : "お客様入力フォーム" }</Grid>
+                <Grid xs={7}></Grid>
+                <Grid xs={1}></Grid>
+            </Grid>
         </div>
     )
 }
