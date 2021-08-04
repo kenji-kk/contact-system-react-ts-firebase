00c73210eb21135d413513a3add6c856ea31d5fb
kenji <kenjix12x22@gmail.com>
2021-08-04 12:31:52 +0900

    LoadingPage作成とログイン画面のui修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 import { db } from "./firebase";
-import './App.css';
-import { Route, useHistory } from 'react-router';
+import "./App.css";
+import { Route, useHistory } from "react-router";
 import { ChatPage } from "./components/pages/ChatPage";
 import { ContactListPage } from "./components/pages/ContactListPage";
 import { NewGuestPage } from "./components/pages/NewGuestPage";
@@ -15,50 +15,51 @@ import { StaffChatPage } from "./components/pages/StaffChatPage";
 import { ContactListNowPage } from "./components/pages/ContactListNowPage";
 import { ContactListCompletePage } from "./components/pages/ContactListCompletePage";
 
-
 export const App: React.VFC = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const history = useHistory();
   const historyContact = () => {
-    history.push('/contactList');
-  }
+    history.push("/contactList");
+  };
   const historyChat = () => {
-    history.push('/chat');
-  }
-  
+    history.push("/chat");
+  };
+
   useEffect(() => {
     const unSub = auth.onAuthStateChanged((authUser) => {
-      
       if (authUser) {
         const docRef = db.collection("users").doc(authUser?.uid);
 
-        docRef.get().then(function(doc) {
+        docRef
+          .get()
+          .then(function (doc) {
             if (doc.exists) {
               dispatch(
                 login({
                   uid: doc.data()?.uid,
                   lastName: doc.data()?.lastName,
-                  firstName: doc.data()?.firstName, 
-                  email: doc.data()?.email, 
-                  tel: doc.data()?.tel, 
-                  productType: doc.data()?.productType, 
-                  staff: doc.data()?.staff, 
-                  timestamp: doc.data()?.timestamp, 
+                  firstName: doc.data()?.firstName,
+                  email: doc.data()?.email,
+                  tel: doc.data()?.tel,
+                  productType: doc.data()?.productType,
+                  staff: doc.data()?.staff,
+                  timestamp: doc.data()?.timestamp,
                   content: doc.data()?.content,
                 })
               );
-              if (doc.data()?.staff){
+              if (doc.data()?.staff) {
                 historyContact();
-              } else if (!doc.data()?.staff){
+              } else if (!doc.data()?.staff) {
                 historyChat();
-              } 
+              }
             } else {
-                console.log("No such document!");
+              console.log("No such document!");
             }
-        }).catch(function(error) {
+          })
+          .catch(function (error) {
             console.log("Error getting document:", error);
-        });
+          });
       } else {
         dispatch(logout());
       }
@@ -70,30 +71,29 @@ export const App: React.VFC = () => {
 
   return (
     <>
-        <Route exact path={'/'}>
-          <NewGuestPage />
-          <LoginGuestPage />
-        </Route>
-        <Route exact path={'/chat'}>
-          <ChatPage />
-        </Route>
-        <Route exact path={'/staffChat/:id'}>
-          <StaffChatPage />
-        </Route>
-        <Route exact path={'/contactList'}>
-          <ContactListPage />
-        </Route>
-        <Route exact path={'/contactListNow'}>
-          <ContactListNowPage />
-        </Route>
-        <Route exact path={'/contactListComplete'}>
-          <ContactListCompletePage />
-        </Route>
-        <Route exact path={'/staff'}>
-          <NewStaffPage />
-          <LoginStaffPage />
-        </Route>
+      <Route exact path={"/"}>
+        <NewGuestPage />
+        <LoginGuestPage />
+      </Route>
+      <Route exact path={"/staff"}>
+        <NewStaffPage />
+        <LoginStaffPage />
+      </Route>
+      <Route exact path={"/chat"}>
+        <ChatPage />
+      </Route>
+      <Route exact path={"/staffChat/:id"}>
+        <StaffChatPage />
+      </Route>
+      <Route exact path={"/contactList"}>
+        <ContactListPage />
+      </Route>
+      <Route exact path={"/contactListNow"}>
+        <ContactListNowPage />
+      </Route>
+      <Route exact path={"/contactListComplete"}>
+        <ContactListCompletePage />
+      </Route>
     </>
-  )
-}
-
+  );
+};
---
+++ src/components/pages/Loading.tsx
@@ -0,0 +1,5 @@
+import React, { memo } from "react";
+
+export const Loading: React.FC = memo(() => {
+  return <div>Loading</div>;
+});
